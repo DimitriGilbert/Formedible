1bf1192bc081a4d0ff2790c123dd3f70ead80d0f
DimitriGilbert <dimitri.gilbert@gmail.com>
2025-07-08 16:32:12 +0200

    same

--- apps/web/src/app/builder/page.tsx
+++ apps/web/src/app/builder/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { motion } from "motion/react";
-import { FormBuilder } from "@/formedible/builder/form-builder";
+import { FormBuilder } from "@/components/formedible/builder/form-builder";
 
 export default function BuilderPage() {
   return (
--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -1,4 +1,4 @@
-export { FormBuilder } from './components/form-builder';
-export { FieldConfigurator } from './components/field-configurator';
-export { FormPreview } from './components/form-preview';
+export { FormBuilder } from './components/formedible/builder/form-builder';
+export { FieldConfigurator } from './components/formedible/builder/field-configurator';
+export { FormPreview } from './components/formedible/builder/form-preview';
 export { cn } from './lib/utils';
